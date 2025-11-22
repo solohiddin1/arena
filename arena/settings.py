c4e9d57d740bd8517255a81d233a1e5affc81458
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-11-22 21:50:37 +0500

    static

--- arena/settings.py
+++ arena/settings.py
@@ -254,6 +254,8 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_FILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
